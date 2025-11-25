ae6c3f5873877b0116470b570a55c56381f59ce4
KGallagher7 <katherine.gallagher@noaa.gov>
2025-11-25 15:35:15 -0500

    Modify directories
    remove _targets directory (defunct) and change RCode directory to R directory for future package development

--- _targets/R/saveTSS.r
+++
@@ -1,6 +0,0 @@
-saveTSS <- function(truth, predicted, ...){
-  pred <- ROCR::prediction(as.vector(abs(predicted)), as.vector(truth))    
-  TSS <- ROCR::performance(pred, "sens", "spec")
-  TSSvals <- max(unlist(TSS@y.values) + unlist(TSS@x.values) - 1)
-  return(TSSvals)
-}
