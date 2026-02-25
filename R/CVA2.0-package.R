4b152998b0f91755fb49450d4a8e38870ba5a062
KGallagher7 <katherine.gallagher@noaa.gov>
2026-02-25 17:37:28 -0500

    Add reporting functions

--- R/CVA2.0-package.R
+++ R/CVA2.0-package.R
@@ -3,7 +3,17 @@
 
 ## usethis namespace: start
 #' @importFrom graphics abline
+#' @importFrom graphics barplot
+#' @importFrom graphics box
 #' @importFrom graphics hist
+#' @importFrom graphics legend
+#' @importFrom graphics mtext
+#' @importFrom graphics par
+#' @importFrom graphics title
+#' @importFrom grDevices dev.off
+#' @importFrom grDevices graphics.off
+#' @importFrom grDevices pdf
+#' @importFrom grDevices windows
 #' @importFrom PredictABEL plotROC
 #' @importFrom stats binomial
 #' @importFrom stats bw.nrd0
@@ -16,6 +26,7 @@
 #' @importFrom stats predict
 #' @importFrom stats sd
 #' @importFrom stats weighted.mean
+#' @importFrom stats weights
 #' @importFrom utils read.csv
 ## usethis namespace: end
 NULL
