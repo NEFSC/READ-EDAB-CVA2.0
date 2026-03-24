88c6503a74f039cc7bd41a9d63bcc72413a56eeb
KGallagher7 <katherine.gallagher@noaa.gov>
2026-03-24 12:26:14 -0400

    Updated function and argument names to snake_case

--- R/normalize_variable_weights.R
+++ R/normalize_variable_weights.R
@@ -1,21 +1,20 @@
 #' @title Normalize Dynamic Variable Weights
 #' @description Normalizes the component model variable importance, as well as the model weights in the final ensemble, to create a weighted average vector of variable importance for the ensemble model. Options allow for static variables, such as time and space, or non-dynamic environmental variables such as bathymetry, to be excluded from the normalization and weighted average.
 #'
-#'
 #' @param vars character vector of variables to generate normalized and averaged ensemble importance for
-#' @param ensWeights vector of component model weights in the ensemble
-#' @param impFlist character vector of file paths to variable importance for component models
+#' @param ens_weights vector of component model weights in the ensemble
+#' @param imp_flist character vector of file paths to variable importance for component models
 #'
 #' @return A vector representing the weighted average of normalized variable importance, representing variable importance in the final ensemble SDM.
 
-combineWeights <- function(vars, ensWeights, impFlist){
+normalize_variable_weights <- function(vars, ens_weights, imp_flist){
   #set up data frame
-  dfI <- as.data.frame(matrix(nrow = length(impFlist), ncol = length(vars)))
+  dfI <- as.data.frame(matrix(nrow = length(imp_flist), ncol = length(vars)))
   colnames(dfI) <- vars
 
   #pull importance vectors and add to data frame
-  for(x in 1:length(impFlist)){
-    load(impFlist[x]) #imp
+  for(x in 1:length(imp_flist)){
+    load(imp_flist[x]) #imp
     if(inherits(imp,'data.frame')){
       imp.vec <- imp$rel.inf ### need to remove spatial variables
       names(imp.vec) <- imp$var
@@ -40,7 +39,7 @@ combineWeights <- function(vars, ensWeights, impFlist){
   dfV <- replace(dfV, is.na(dfV), 0)
   dfV <- t(apply(dfV, 1, FUN = function(x){x/sum(x,na.rm = T)}))
   dfV <- replace(dfV, is.na(dfV), 0)
-  ws <- apply(dfV, MARGIN = 2, FUN = weighted.mean, w = ensWeights, na.rm = T) #weighted average of weights
+  ws <- apply(dfV, MARGIN = 2, FUN = weighted.mean, w = ens_weights, na.rm = T) #weighted average of weights
 
   return(ws)
 }
