b3ffd7e7d16747efa0446b7ade7a93c3c95b2c2c
Katherine Gallagher <katherine.gallagher@noaa.gov>
2026-07-09 12:48:02 -0500

    Update standard deviation and normalization methods to add option for spatial/temporal calculations vs "global" calculations. Also updated to terra when possible

--- R/normalize_model_data.R
+++ R/normalize_model_data.R
@@ -2,43 +2,37 @@
 #' @description
 #' Normalize model data across the entire provided timeseries using a z-score
 #'
-#' @param raw_list List of output rasters from \code{pull_hind} or \code{pull_forecast}. Alternatively, a list of rasters containing gridded data to be normalized, with each entry in the list containing a rasterStack of a timeseries of environmental data to be normalized.
-#' @param avg_list,sd_list List of output rasters from \code{avg_model_data} and \code{sd_env}, respectively. Alternatively, averages and standard deviations of desired gridded datasets. Should be a list of rasterStacks, with each entry in the list containing the rasterStack for a different variable.
-#' @param short_names vector of simplified variable names to help name resulting raster files.
+#' @param raw Output spatRaster from \code{pull_mom6_hindcast} or \code{pull_mom6_forecast}. Alternatively, a spatRaster of data to be normalized
+#' @param avg,sd Output spatRaster from \code{avg_model_data} and \code{sd_model_data}, respectively. Alternatively, averages and standard deviations of desired gridded datasets. 
+#' @param spatial.temporal TRUE/FALSE to determine how normalization should occur. If TRUE, calculations should be spatially and temporally explicit. \code{avg} and \code{sd} should be spatRaster objects with averages and standard deviations of data in space and time. If FALSE, the calculation occurs across space and time; \code{avg} and \code{sd} should be single values representing the overall average and standard deviation for the given variable.
 #'
-#' @return a list whose length is equal to the length of \code{raw_list}, where each item in the list is a rasterStack of normalized data associated with that variable
-#'
-#'@export
+#' @return a spatRaster of normalized environmental data
 
-normalize_model_data <- function(raw_list, avg_list, sd_list, short_names) {
-  normList <- vector(mode = 'list', length = length(raw_list))
 
-  for (x in 1:length(raw_list)) {
-    v <- raw_list[[x]]
-    avgs <- avg_list[[x]]
-    sds <- sd_list[[x]]
-
-    ##normalize data to monthly averages
-    mth <- rep(1:12, times = raster::nlayers(v) / 12) #creates repeating list of 1:12 for each year
+normalize_model_data <- function(raw, avg, sd, spatial.temporal){
+  
+  if(spatial.temporal){
+    ##normalize data to monthly spatially and temporally explicit averages and sds 
+    mth <- rep(1:12, times = raster::nlayers(v)/12) #creates repeating list of 1:12 for each year
     #normalize data
     norm <- NULL
-    for (m in 1:raster::nlayers(v)) {
+    for(m in 1:raster::nlayers(raw)){
       #subset both rasterbricks
-      subX <- raster::subset(v, m)
-      subA <- raster::subset(avgs, mth[m])
-      subS <- raster::subset(sds, mth[m])
-
+      subX <- raster::subset(raw,m)
+      subA <- raster::subset(avg,mth[m])
+      subS <- raster::subset(sd,mth[m])
+      
       nm <- (subX - subA) / subS
       norm <- abind::abind(as.array(nm), norm, along = 3)
     } #end m
-    norm <- raster::brick(norm)
-    raster::extent(norm) <- raster::extent(v) #make the extent the same as v
-    raster::crs(norm) <- raster::crs(v)
-    names(norm) <- names(v) #set names
-    normList[[x]] <- norm #save
-  } #end x
-
-  names(normList) <- short_names #set names of lists
-
-  return(normList)
+    norm <- terra::rast(norm)
+    terra::ext(norm) <- terra::ext(raw) #make the extent the same as v
+    terra::crs(norm) <- terra::crs(raw)
+  } else {
+    #use averages/standard deviations calculated across space and time (single values)
+    norm <- terra::app(raw, fun = function(x){(x - avg) / sd})
+  }
+  
+  return(norm)
+  
 } #end function
--- R/sd_model_data.R
+++ R/sd_model_data.R
@@ -1,37 +1,33 @@
 #' @title Calculate Standard Deviation on MOM6 Data
 #' @description
-#' Calculate monthly standard deviations from raw model data across the entire provided timeseries. This is built specifically for MOM6 output, but would work on any rasterStack of gridded data.
 #'
-#' @param raw_list List of output rasters from \code{pull_mom6_hindcast} or \code{pull_mom6_forecast}
-#'
-#' @return a list whose length is equal to the number of variables supplied, where each item in the list is a rasterStack of data associated with that variable
-#'
-#'@export
+#' @param raw spatRaster from \code{pull_mom6_hindcast} or \code{pull_mom6_forecast}
+#' @param spatial.temporal TRUE/FALSE to determine how standard deviations are calculated If TRUE, standard deviation calculations are spatially and temporally explicit. If FALSE, the calculation occurs across space and time
+#' 
+#' @return If \code{spatial.temporal} is TRUE, a spatRaster is returned. If FALSE, a single value is returned. 
 
-sd_model_data <- function(raw_list) {
-  sdList <- vector(mode = 'list', length = length(raw_list))
-
-  for (x in 1:length(raw_list)) {
-    v <- raw_list[[x]]
-
-    ## create monthly averages
+sd_model_data <- function(raw, spatial.temporal){
+  
+  if(spatial.temporal){
+    ## create monthly averages across space and time (months - should make this customizable at some point)
     sds <- NULL
     for (m in 1:12) {
-      mn <- seq(m, raster::nlayers(v), by = 12) #grab all month xs from timeseries by creating a sequence
-      MNS <- raster::subset(v, mn) #subset raster brick
-      mm <- raster::calc(MNS, fun = sd, na.rm = T) #average
+      mn <- seq(m, terra::nlyr(v), by = 12) #grab all month xs from timeseries by creating a sequence
+      MNS <- v[[mn]] #subset raster brick
+      mm <- terra::stdev(MNS, na.rm = T) #average
       sds <- abind::abind(as.array(mm), sds, along = 3) #make array and bind together
       #print(m)
     } #end m
     #convert to rasterBrick
-
-    sds <- raster::brick(sds)
-    raster::extent(sds) <- raster::extent(v) #make the extent the same as v
-    raster::crs(sds) <- raster::crs(v)
+    
+    sds <- terra::rast(sds)
+    terra::ext(sds) <- terra::ext(v) #make the extent the same as v
+    terra::crs(sds) <- terra::crs(v)
     names(sds) <- 1:12
-    sdList[[x]] <- sds
-  } #end x
-
-  #names(sdList) <- names(rastList)
-  return(sdList)
+  } else {
+    #calculate global average across layers and space
+    sds <- terra::global(v, 'sd', na.rm = T)
+  } #end if spatial.temporal
+  
+  return(sds)
 } #end function
