0e5a98378985fdb676291925648ff0fd607cff83
KGallagher7 <katherine.gallagher@noaa.gov>
2026-08-10 15:55:47 -0400

    docs: updated argument descriptions

--- R/make_variable_exposure.R
+++ R/make_variable_exposure.R
@@ -3,10 +3,10 @@
 #' @description Combines raw variable exposures and SDM results, using the SDM results as weights to average the exposure across time to produce maps of species-specific exposure for each variable or across space to produce timeseries of species-specific exposure for each variable
 #'
 #' @param type designates desired output, must equal 'map' or 'timeseries'
-#' @param ranked_exposure output from \code{rank_exposure}
-#' @param sdm_raster rasterStack with monthly averages of SDM results to use as weights for weighted average
+#' @param ranked_exposure spatRaster of ranked exposure data. Output from \code{rank_exposure}
+#' @param sdm_raster spatRaster with monthly averages of SDM results to use as weights for weighted average
 #'
-#' @return If \code{type == 'map'}, a rasterStack with the number of layers equal to the number of variables supplied, containing the weighted average exposure, weighted by SDM results, for each variable. If \code{type == 'timeseries'}, a matrix with the number of rows equal to the number of variables supplied, and 12 columns (1 for each month) containing the weighted average exposure, weighted by SDM results, for each variable.
+#' @return If \code{type == 'map'}, a spatRaster with the number of layers equal to the number of variables supplied, containing the weighted average exposure, weighted by SDM results, for each variable. If \code{type == 'timeseries'}, a matrix with the number of rows equal to the number of variables supplied, and 12 columns (1 for each month) containing the weighted average exposure, weighted by SDM results, for each variable.
 #'
 #'@export
 
@@ -16,7 +16,7 @@ make_variable_exposure <- function(type, ranked_exposure, sdm_raster) {
     for (x in 1:length(ranked_exposure)) {
       s <- ranked_exposure[[x]]
 
-      mapExp[[x]] <- stats::weighted.mean(
+      mapExp[[x]] <- terra::weighted.mean(
         raster::stack(s),
         w = sdm_raster,
         na.rm = T
