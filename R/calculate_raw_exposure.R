bf824cc56456f6d83bcce6b38b6f6fcb903a0a36
KGallagher7 <katherine.gallagher@noaa.gov>
2026-08-13 10:00:19 -0400

    feature: update function to work on single raster layers to be more similar to hindcast data workflows. used existing avg/sd functions and added options for spatio-temporal averaging and bathymetry masking

--- R/calculate_raw_exposure.R
+++ R/calculate_raw_exposure.R
@@ -2,59 +2,39 @@
 #' @description
 #' Calculates the raw variables exposure as the ratio of the difference between future and present conditions, divided by present standard deviations (a z-score).
 #'
-#' @param present_rasters,future_rasters lists of rasterStacks, representing the present and future timeseries to calculate exposure with, with each item in the list corresponding to an environmental variable. Both lists need to have the same length (aka number of variables) but the lengths of the timeseries (equal to the number of layers in rasterStacks) can differ. Function assumes that the rasters are monthly timeseries and will average monthly.
-#' @return a list of rasterStacks with each layer representing monthly raw exposure. The length of the list is equal to the length of the lists supplied as \code{present_rasters} and \code{future_rasters}
+#' @param present,future spatRasters, representing the present and future timeseries to calculate exposure with. Function assumes that the rasters are monthly timeseries and will average monthly.
+#' @param spatial_temporal TRUE/FALSE to determine method for averaging present data ONLY. If TRUE, normalization is spatially and temporally explicit. If FALSE, normalization occurs using averages/standard deviations calculated across space and time. Future datasets are always averaged across time and space to ensure that exposure value is spatially explicit. 
+#' @param mask_bathy TRUE/FALSE indicating whether or not to use bathymetry data as a mask for raw data
+#' @param bathy a spatRaster of bathymetry data. Must be same extent as results from pull_mom6_hindcast/forecast
+#' @param bathy_range a vector containing the minimum and maximum desired bathymetry values 
+#' @return a spatRaster of monthly raw exposure 
 #'
 #'@export
 
-calculate_raw_exposure <- function(present_rasters, future_rasters) {
-  EXP <- vector(mode = 'list', length = length(future_rasters))
-  for (v in 1:length(future_rasters)) {
-    ex <- vector(mode = 'list', length = 12)
-    for (x in 1:12) {
-      #take mean of 'present' and 'future'
-      mPres <- raster::calc(
-        raster::subset(
-          present_rasters[[v]][[1]],
-          seq(x, raster::nlayers(present_rasters[[v]][[1]]), by = 12)
-        ),
-        mean
-      )
-      mFut <- raster::calc(
-        raster::subset(
-          future_rasters[[v]][[1]],
-          seq(x, raster::nlayers(future_rasters[[v]][[1]]), by = 12)
-        ),
-        mean
-      )
-
-      #difference in means
-      mDiff <- mFut - mPres
-
-      ##calculate SD
-      sdPres <- raster::calc(
-        raster::subset(
-          present_rasters[[v]][[1]],
-          seq(x, raster::nlayers(present_rasters[[v]][[1]]), by = 12)
-        ),
-        sd
-      )
-      sdFut <- raster::calc(
-        raster::subset(
-          future_rasters[[v]][[1]],
-          seq(x, raster::nlayers(future_rasters[[v]][[1]]), by = 12)
-        ),
-        sd
-      )
-
-      #calculate exposure
-      ex[[x]] <- raster::stack(mDiff) / raster::stack(sdPres)
+calculate_raw_exposure <- function(present, future, spatial_temporal, mask_bathy, bathy, bathy_range) {
+ 
+   if (mask_bathy) {
+    #Unwrap the bathymetry raster inside the worker
+    if (inherits(bathy, "PackedSpatRaster")) {
+      bathy <- terra::unwrap(bathy)
     }
-
-    #stack
-    EXP[[v]] <- raster::stack(ex)
-    print(v)
+    #reproject because there are slight differences in resolution/extent for some reason, especially with the forecasts
+    bathy_aligned <- terra::resample(bathy, present, method = "bilinear")
+    
+    present <- terra::ifel(bathy_aligned <= bathy_range[1] | bathy_aligned > bathy_range[2], NA, present)
+    future <- terra::ifel(bathy_aligned <= bathy_range[1] | bathy_aligned > bathy_range[2], NA, future)
   }
-  names(EXP) <- names(future_rasters)
+  
+  #calculate averages
+  pAvg <- avg_model_data(present, spatial_temporal = spatial_temporal) #can change whether z-score is calculated relative to spatially/temporally explicit present averages or global averages 
+  
+  fAvg <- avg_model_data(future, spatial_temporal = TRUE) #always want future averages done across time and space (create monthly, spatially-explicit averages) so that exposure calculation is spatially/temporally explicit
+  
+  #calculate present SD 
+  pSD <- sd_model_data(present, spatial_temporal)
+
+  #calculate exposure
+  EXP <- (fAvg - pAvg) / pSD
+  
   return(EXP)
 }
