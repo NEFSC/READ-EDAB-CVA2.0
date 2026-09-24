ca93a69ea09df1b3c4e7258e2717141eddc7e0ac
KGallagher7 <katherine.gallagher@noaa.gov>
2026-09-24 11:34:41 -0400

    feature: add id_col argument to change unique identifier; allows for global sensitivity to be calculated using the species column rather than relying on the stock.name column

--- R/calculate_data_quality.R
+++ R/calculate_data_quality.R
@@ -2,6 +2,7 @@
 #' @description Calculates mean data quality score
 #'
 #' @param species_attributes a data frame containing all of the expert scores for one species. See example for how to generate this list from the FCVA output.
+#' @param id_col column name to use as unique identifier for row names 
 #'
 #' @return a vector of mean data quality scores for each attribute with the length of the number of attributes.
 #'
@@ -18,7 +19,7 @@
 #'
 #'@export
 
-calculate_data_quality <- function(species_attributes) {
+calculate_data_quality <- function(species_attributes, id_col) {
   #calculate mean data quality
   dq <- stats::aggregate(
     Data.Quality ~ Attribute.Name,
@@ -29,6 +30,6 @@ calculate_data_quality <- function(species_attributes) {
   #format data to fit into eventual csv
   dq <- as.data.frame(t(dq[, 2]))
   colnames(dq) <- unique(species_attributes$Attribute.Name)
-  rownames(dq) <- unique(species_attributes$Stock.Name)
+  rownames(dq) <- unique(species_attributes[,id_col])
   return(dq)
 }
